632df4936d5cce4cfdf30cc0ac674a62f92b2143
Abdulrahman-Abdulhamid <91764016+Abdulrahman-Abdulhamid@users.noreply.github.com>
2023-02-08 10:18:01 -0500

    Added the text -> Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p className="hello-world">Hello World</p>
         </div>
     );
 }
